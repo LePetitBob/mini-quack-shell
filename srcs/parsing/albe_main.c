033a70c0593242185b3a0127d3120cfa573487e2
Albe MARINI-AUDOUARD <amarini-@e2r10p11.clusters.42paris.fr>
2022-02-08 19:04:08 +0100

    📝 -COntinued doc for the parsing, implemented basic main (which needs completion) i need to see how to do the splits for the first part of the parsing (which is to split everything, but beware of the "' and the \'\")

--- srcs/parsing/albe_main.c
+++ srcs/parsing/albe_main.c
@@ -6,8 +6,28 @@
 /*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 11:38:25 by amarini-          #+#    #+#             */
-/*   Updated: 2022/02/08 12:48:52 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/02/08 18:16:52 by amarini-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_quack_shell.h"
+
+int	main(int ac, char *av, char *env[])
+{
+	char	*line;
+
+	(void)ac;
+	(void)av;
+	while (1)
+	{
+		line = readline("mini-quack-shell$ ");
+		
+	}
+}
+
+void	split_line(char *line)
+{
+	char	**args;
+
+	
+}
