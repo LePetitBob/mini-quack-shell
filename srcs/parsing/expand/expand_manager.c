b531cc4123525fce1d19db7eeddc38ccb524c357
Albe MARINI-AUDOUARD <amarini-@e2r11p15.clusters.42paris.fr>
2022-02-11 19:14:18 +0100

    ✨🚧 -Started implementing Expand

---
+++ srcs/parsing/expand/expand_manager.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand_manager.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/11 15:03:12 by amarini-          #+#    #+#             */
+/*   Updated: 2022/02/11 18:23:30 by amarini-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mini_quack_shell.h"
+
+void	expand_manager(t_token *tokens)
+{
+	t_token	*it;
+	int		i;
+
+	printf("started Expand\n");
+	it = tokens;
+	while (it)
+	{
+		printf("it->str-[%s]\n", it->str);
+		i = 0;
+		if (it->type == WORD)
+			while (it->str[i] != '\0')
+			{
+				if (it->str[i] == '\'' || it->str[i] == '\"'
+					|| it->str[i] == '$')
+				{
+					printf("gonna expand\n");
+					expand_str(it, i);
+					break ;
+				}
+				++i;
+			}
+		it = it->next;
+	}
+	printf("finished Expand\n");
+}
+
+void	expand_str(t_token *it, int index)
+{
+	char	**ret;
+	char	*tmp;
+	int		i;
+
+	ret = NULL;
+	i = 0;
+	if (index > 0)
+	{
+		tmp = ft_strndup(it->str, index);
+		ret = ft_add_tab(ret, tmp);
+		free(tmp);
+		tmp = (char *)malloc((ft_strlen(it->str) - index + 1) * sizeof(char));
+		if (!tmp)
+			return ;
+		tmp[ft_strlen(it->str) - index] = '\0';
+		while (i < ft_strlen(it->str) - index)
+		{
+			tmp[i] = it->str[index + i];
+			++i;
+		}
+		free(it->str);
+		it->str = ft_strdup(tmp);
+		free(tmp);
+	}
+	//split str into -> ' " $
+	split_quotes_dollar(it->str, &ret);
+	ft_print_tab(ret);
+	//look at index
+		//if '
+			//split ' with '
+		//if "
+			//split " with "
+		//if $
+			//split $ until non alnum char || _
+
+		//for ' -> del quotes | turn spaces into *-1
+		//for " -> del quotes | get env var $
+		//for $ -> get env var $
+}
---
+++ srcs/parsing/expand/split_quotes_dollar.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   split_quotes_dollar.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/11 16:37:28 by amarini-          #+#    #+#             */
+/*   Updated: 2022/02/11 19:02:15 by amarini-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mini_quack_shell.h"
+
+void	split_quotes_dollar(char *str, char *(**ret))
+{
+	char	*tmp;
+	char	quote;
+	int		i;
+
+	i = 0;
+	tmp = ft_strdup(str);
+	ft_bzero(tmp, ft_strlen(str));
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\'' || str[i] == '\"')
+		{
+			quote = str[i];
+			++i;
+			while (str[i] != '\0' && str[i] != quote)
+			{
+				tmp[ft_strlen(tmp)] = str[i];
+				++i;
+			}
+			if (quote == '\'')
+				negate_spaces(&tmp);
+			*ret = ft_add_tab(*ret, tmp);
+			ft_bzero(tmp, ft_strlen(str));
+		}
+		else if (str[i] == '$')
+		{
+			tmp[ft_strlen(tmp)] = str[i];
+			++i;
+			if (ft_isalpha(str[i]) == 0)
+			{
+				tmp[ft_strlen(tmp)] = str[i];
+				printf("char after $ is not alpha\n");
+			}
+			while (str[i] != '\0' && (ft_isalnum(str[i]) == 1 || str[i] == '_'))
+			{
+				tmp[ft_strlen(tmp)] = str[i];
+				++i;
+			}
+			*ret = ft_add_tab(*ret, tmp);
+			ft_bzero(tmp, ft_strlen(str));
+		}
+		else
+			++i;
+	}
+	free(tmp);
+}
+
+void	negate_spaces(char *(*str))
+{
+	int	i;
+
+	i = 0;
+	while ((*str)[i] != '\0')
+	{
+		if ((*str)[i] == ' ')
+			(*str)[i] *= -1;
+		++i;
+	}
+}
