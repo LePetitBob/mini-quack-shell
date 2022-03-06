00c7535a16c35c0bf8dc5d6dd060fcea2e1f0a08
Vincent DURIEZ <vduriez@e3r9p4.clusters.42paris.fr>
2022-03-06 18:31:27 +0100

    🐛 fixed cmd not found/no such file or dir

--- srcs/cmd_manager.c
+++ srcs/cmd_manager.c
@@ -6,7 +6,7 @@
 /*   By: vduriez <vduriez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 19:37:04 by amarini-          #+#    #+#             */
-/*   Updated: 2022/03/06 17:46:26 by vduriez          ###   ########.fr       */
+/*   Updated: 2022/03/06 17:58:09 by vduriez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,10 +57,8 @@ void	close_wait_clear(t_cmd_lst *cmds, int fd[6])
 	while (tmp)
 	{
 		waitpid(tmp->pid, &err, 0);
-		if (WIFEXITED(err) && (cmds->first->next))//&& (ft_strcmp(tmp->arg->str, "export") && ft_strcmp(tmp->arg->str, "unset")))
-		{
+		if (WIFEXITED(err) && (cmds->first->next))
 			g_status.exit_status = WEXITSTATUS(err);
-		}
 		tmp = tmp->next;
 	}
 	rm_cmds(cmds);
