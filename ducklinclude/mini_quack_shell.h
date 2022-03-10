fe5c3792830f083827d001208d13b366310665a1
vduriez <vduriez@student.42.fr>
2022-03-10 19:23:19 +0100

    🩹 - history now updates only if line isn't empty or has only spaces in it

--- ducklinclude/mini_quack_shell.h
+++ ducklinclude/mini_quack_shell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mini_quack_shell.h                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amarini- <amarini-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vduriez <vduriez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 07:22:11 by amarini-          #+#    #+#             */
-/*   Updated: 2022/03/10 14:42:30 by amarini-         ###   ########.fr       */
+/*   Updated: 2022/03/10 19:01:48 by vduriez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,6 +117,7 @@ t_token		*get_last_token(t_token *tokens);
 
 //?			Parsing
 //			Split
+int			is_empty(char *s);
 void		split_manager(char *line, t_env *env);
 int			syntax_errors(char **args);
 void		misc_error(char *str, char pb, int *index);
